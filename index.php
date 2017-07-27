09dab0a57d1c07bda5ea6145fc943632dbe81d8c
Prince Adeyemi <prince@vegasnewspaper.com>
2017-07-26 23:42:39 -0700

    Start with index

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
-ECHO is on.
- "Elho" 
+<?php
+
+	/**
+	
+	
+	
+	
+	
+	
+	
+
+
+
+		$hotel = new HotelBooking( 'Las Vegas, NV', '2017-01-01', '2017,-02-01' );
+		var_dump( $hotel->_results );
+	
+	*/
+
+
+?>
