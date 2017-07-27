da989d87bca88fa4967ef0bfd78e353cfc125aec
Prince Adeyemi <prince@vegasnewspaper.com>
2017-07-26 23:44:53 -0700

    added elho to index

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
 	
 	
 	
-	
+		
 	
 
-
+		usage:
+		
+		include_once("HotelBooking.php");
 
 		$hotel = new HotelBooking( 'Las Vegas, NV', '2017-01-01', '2017,-02-01' );
 		var_dump( $hotel->_results );
